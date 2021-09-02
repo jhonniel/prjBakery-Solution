c04caa4d2b8a4c040c5345ab7e40348be38db04d
unknown <jhonnielygay99@gmail.com>
2021-09-02 21:02:31 +0800

    prjBakery

---
+++ prjBakerySolution_Ygay/frmMain.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        Me.txtDoughnuts = New System.Windows.Forms.TextBox()
+        Me.txtMuffins = New System.Windows.Forms.TextBox()
+        Me.lblTotalItems = New System.Windows.Forms.TextBox()
+        Me.lblTotalSales = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(118, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Sales Receipt"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(119, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Date"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(26, 21)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(75, 64)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(56, 127)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Doughtnuts:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 157)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Muffins:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(56, 179)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Total items:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(56, 205)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Total sales:"
+        '
+        'txtDate
+        '
+        Me.txtDate.Location = New System.Drawing.Point(155, 74)
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(100, 20)
+        Me.txtDate.TabIndex = 7
+        '
+        'txtDoughnuts
+        '
+        Me.txtDoughnuts.Location = New System.Drawing.Point(122, 124)
+        Me.txtDoughnuts.Name = "txtDoughnuts"
+        Me.txtDoughnuts.Size = New System.Drawing.Size(40, 20)
+        Me.txtDoughnuts.TabIndex = 8
+        '
+        'txtMuffins
+        '
+        Me.txtMuffins.Location = New System.Drawing.Point(122, 150)
+        Me.txtMuffins.Name = "txtMuffins"
+        Me.txtMuffins.Size = New System.Drawing.Size(40, 20)
+        Me.txtMuffins.TabIndex = 9
+        '
+        'lblTotalItems
+        '
+        Me.lblTotalItems.Location = New System.Drawing.Point(122, 176)
+        Me.lblTotalItems.Name = "lblTotalItems"
+        Me.lblTotalItems.Size = New System.Drawing.Size(40, 20)
+        Me.lblTotalItems.TabIndex = 10
+        '
+        'lblTotalSales
+        '
+        Me.lblTotalSales.Location = New System.Drawing.Point(122, 202)
+        Me.lblTotalSales.Name = "lblTotalSales"
+        Me.lblTotalSales.Size = New System.Drawing.Size(65, 20)
+        Me.lblTotalSales.TabIndex = 11
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(215, 100)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(89, 24)
+        Me.btnCalc.TabIndex = 12
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(215, 127)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(89, 24)
+        Me.btnPrint.TabIndex = 13
+        Me.btnPrint.Text = "Print Receipt"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(215, 156)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(89, 24)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clear Screen"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(215, 186)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(89, 24)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblMsg)
+        Me.GroupBox1.Location = New System.Drawing.Point(59, 240)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(245, 59)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Location = New System.Drawing.Point(72, 16)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(0, 13)
+        Me.lblMsg.TabIndex = 0
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 314)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblTotalSales)
+        Me.Controls.Add(Me.lblTotalItems)
+        Me.Controls.Add(Me.txtMuffins)
+        Me.Controls.Add(Me.txtDoughnuts)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Enabled = False
+        Me.Name = "frmMain"
+        Me.Text = "Mayer'S Purple Bakery"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtDate As TextBox
+    Friend WithEvents txtDoughnuts As TextBox
+    Friend WithEvents txtMuffins As TextBox
+    Friend WithEvents lblTotalItems As TextBox
+    Friend WithEvents lblTotalSales As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblMsg As Label
+End Class
---
+++ prjBakerySolution_Ygay/frmMain.vb
@@ -0,0 +1,78 @@
+﻿Option Explicit On
+Option Infer On
+Option Strict On
+
+Public Class frmMain
+    Private strClerk As String
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Const decITEM_PRICE As Decimal = 0.5D
+        Const decTAX_RATE As Decimal = 0.02D
+        Dim intDonuts As Integer
+        Dim intMuffins As Integer
+        Dim intTotalItems As Integer
+        Dim decSubtotal As Decimal
+        Dim decSalesTax As Decimal
+        Dim decTotalSales As Decimal
+
+        Integer.TryParse(txtDoughnuts.Text, intDonuts)
+        Integer.TryParse(txtMuffins.Text, intMuffins)
+        intTotalItems = intDonuts + intMuffins
+
+        decSubtotal = intTotalItems * decITEM_PRICE
+
+        decSalesTax = decSubtotal * decTAX_RATE
+
+        decTotalSales = decSubtotal + decSalesTax
+
+        lblTotalItems.Text = Convert.ToString(intTotalItems)
+        lblTotalSales.Text = Convert.ToString(decTotalSales)
+        lblMsg.Text = decSalesTax.ToString("C2")
+
+        lblMsg.Text = "The sale tax was " & decSalesTax.ToString("C2") & "." &
+            ControlChars.NewLine & strClerk
+
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Const strPROMT As String = "Salesclerk's name:"
+        Const strTITLE As String = "Name Entry"
+
+        strClerk = InputBox(strPROMT, strTITLE)
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDoughnuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+
+        txtDoughnuts.Focus()
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        btnCalc.Visible = False
+        btnClear.Visible = False
+        btnExit.Visible = False
+        btnPrint.Visible = False
+        ' PrintForm1.Print()
+        btnCalc.Visible = True
+        btnClear.Visible = True
+        btnExit.Visible = True
+        btnPrint.Visible = True
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub txtDate_TextChanged(sender As Object, e As EventArgs) Handles txtDate.TextChanged
+
+
+    End Sub
+End Class
